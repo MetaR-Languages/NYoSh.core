f5128ac69031336d57cc55c3ac4c211b827e3986
Manuele Simi <manuele.simi@campagnelab.org>
2018-11-14 12:04:08 -0500

    Add Util to the list of plugins to deploy on artifactory. Allow to upload only a subset of the plugins.

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -31,12 +31,15 @@ function deploy_artifact {
 
 function deploy_all_artifacts {
    maven_bin=$1/mvn
-   deploy_artifact $maven_bin UI org.campagnelab.mps.UI/*.zip && \
-   deploy_artifact $maven_bin TextOutput org.campagnelab.TextOutput/*.zip && \
-   deploy_artifact $maven_bin Logger org.campagnelab.Logger/*.zip && \
-   deploy_artifact $maven_bin Background org.campagnelab.Background/*.zip && \
-   deploy_artifact $maven_bin ClusterConfig org.campagnelab.ClusterConfig/*.zip && \
-   deploy_artifact $maven_bin NYoSh org.campagnelab.NYoSh/*.zip #&& \
-   deploy_artifact $maven_bin GobyWeb org.campagnelab.GobyWeb/*.zip && \
-   deploy_artifact $maven_bin Interactive org.campagnelab.Interactive/*.zip
+   deploy_artifact $maven_bin UI org.campagnelab.mps.UI/*.zip || echo "WARNING: UI has NOT been deployed"
+   deploy_artifact $maven_bin TextOutput org.campagnelab.TextOutput/*.zip || echo "WARNING: TextOutput has NOT been deployed"
+   deploy_artifact $maven_bin Logger org.campagnelab.Logger/*.zip || echo "WARNING: Logger has NOT been deployed"
+   deploy_artifact $maven_bin Background org.campagnelab.Background/*.zip || echo "WARNING: Background has NOT been deployed"
+   deploy_artifact $maven_bin ClusterConfig org.campagnelab.ClusterConfig/*.zip || echo "WARNING: ClusterConfig has NOT been deployed"
+   deploy_artifact $maven_bin NYoSh org.campagnelab.NYoSh/*.zip || echo "WARNING: NYoSh has NOT been deployed"
+   deploy_artifact $maven_bin GobyWeb org.campagnelab.GobyWeb/*.zip || echo "WARNING: GobyWeb has NOT been deployed"
+   deploy_artifact $maven_bin Interactive org.campagnelab.Interactive/*.zip|| echo "WARNING: Interactive has NOT been deployed"
+   deploy_artifact $maven_bin Util org.campagnelab.Util/*.zip || echo "WARNING: Util has NOT been deployed"
+
+
 }
